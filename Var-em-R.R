ecc75bd3358e9746a12335a78d0a6b683841b1f0
bruno <bruno.3av@gmail.com>
2021-02-26 13:29:54 -0300

    learned variables in R

---
+++ Var-em-R.R
@@ -0,0 +1,33 @@
+
+
+var1 = 100
+var1
+mode(var1) ## tipo
+help(mode)
+sqrt(var1)
+
+var2 = var1
+print(var2)
+typeof(var2)
+help(typeof)
+
+var3 = c("primeiro", "segundo", "terceiro")
+print(var3)
+
+var4 = function(x) {x+3}
+var4(5)
+mode(var4)
+
+var5 = as.character(var1)
+var5
+
+x <- c(1, 2, 3)
+x
+x[1]
+
+ls()
+
+rm(x)
+x
+
+
