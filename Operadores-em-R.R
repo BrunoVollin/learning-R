a33bae8e2aacf7b6d6badc44c86f71809b0ce435
bruno <bruno.3av@gmail.com>
2021-02-26 13:08:38 -0300

    learned operators in R

--- Operadores-em-R.R
+++ Operadores-em-R.R
@@ -5,3 +5,39 @@ getwd()
 
 ##soma 
 4 + 5
+
+##sub
+7 - 4
+
+##multi
+2 * 8
+
+##div
+3 / 3
+
+##pot
+3^2
+3**2
+
+##mod
+16 %% 3
+
+### Operadores relacionais
+
+##variaveis
+x = 7
+y = 5
+
+x > 8
+x < 8
+x <= 8
+x >= 8
+
+## lógicos
+( x == 8) & (x > y) ##and
+
+( x == 8) | (x > y) ##or
+
+print(!x < 8) ##not
+
+
